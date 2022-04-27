f8c1f38a1d55951d4f2224f6b4701e5bb2c1bb70
Nick <60210397+Ramcalt@users.noreply.github.com>
2022-04-27 18:37:47 +1000

    Create ir_sensor.py
    opps forgot to add this in

---
+++ build/modules/ir_sensor.py
@@ -0,0 +1,26 @@
+from machine import ADC
+
+
+class Sensor:
+    def __init__(self, analog_pin):
+        """Initialise IR Sensor object"""
+        self.adc = ADC(analog_pin)
+
+        # sensitivity constants
+        self.SENSITIVITY = 4000  # any analog voltage above this is considered 'road'
+
+    def set_sensitivity(self, sensitivity):
+        self.SENSITIVITY = sensitivity
+
+    def get_sensitivity(self):
+        return self.SENSITIVITY
+
+    def is_on_road(self):
+        reading = self.reading()
+        if reading > self.SENSITIVITY:
+            return True
+        else:
+            return False
+
+    def reading(self):
+        return self.adc.read_u16()  # value between 0 - 65535
